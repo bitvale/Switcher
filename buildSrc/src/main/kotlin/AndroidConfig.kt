ab6d238cb31899e55e44f1b5967e3ed26c8d0d9f
bitvale <jquickapp@gmail.com>
2022-01-03 00:02:05 +0200

    Update deps, API 31

--- buildSrc/src/main/kotlin/AndroidConfig.kt
+++ buildSrc/src/main/kotlin/AndroidConfig.kt
@@ -1,10 +1,10 @@
 object AndroidConfig {
-    const val COMPILE_SDK_VERSION = 30
+    const val COMPILE_SDK_VERSION = 31
     const val MIN_SDK_VERSION = 17
-    const val TARGET_SDK_VERSION = 30
+    const val TARGET_SDK_VERSION = 31
 
     const val VERSION_CODE = 1
-    const val VERSION_NAME = "1.1.1"
+    const val VERSION_NAME = "1.1.2"
 
     const val ID = "com.bitvale.switcher"
     const val TEST_INSTRUMENTATION_RUNNER = "androidx.test.runner.AndroidJUnitRunner"
